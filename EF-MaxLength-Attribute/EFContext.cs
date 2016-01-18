462ac82e94c108e3ca8513fd94b3d2381c7a573d
TekTutorialsHub <admin@TekTutorialsHub.com>
2016-01-18 21:02:09 +0530

    Created

---
+++ EF-MaxLength-Attribute/EFContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace EF_MaxLength_Attribute
+{
+    public class EFContext : DbContext
+    {
+        public EFContext()
+            : base("EFDatabase")
+        {
+            Database.SetInitializer<EFContext>(new DropCreateDatabaseIfModelChanges<EFContext>());
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+
+    }
+
+
+}
---
+++ EF-MaxLength-Attribute/models.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EF_MaxLength_Attribute
+{
+
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+        [MaxLength(50)]
+        public string Name { get; set; }
+        [MinLength(10)]
+        public string Address { get; set; }
+
+    }
+
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+        
+        [MaxLength(50,ErrorMessage="Name cannot be greater than 50")]
+        public string Name { get; set; }
+
+        [MinLength(10, ErrorMessage = "Name cannot be less than 10")]
+        public string Address { get; set; }
+
+    }
+
+
+
+}
